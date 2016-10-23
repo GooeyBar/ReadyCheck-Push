5cd825f22a73e34c5ad56eeb756ffc510eb529c7
Daniel Tsioni <dtsioni@gmail.com>
2016-10-22 23:10:17 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+var firebase = require('firebase');
+var request = require('request');
+require('dotenv').config();
+var API_KEY = process.env.API_KEY; // Your Firebase Cloud Server API key
+
+firebase.initializeApp({
+  serviceAccount: "ReadyCheck-98fcd0c0a273.json",
+  databaseURL: "https://readycheck-24872.firebaseio.com/"
+});
+ref = firebase.database().ref();
+
+function listenForNotificationRequests() {
+  var requests = ref.child('notificationRequests');
+  requests.on('child_added', function(requestSnapshot) {
+    var request = requestSnapshot.val();
+    sendNotificationToUser(
+      request.username, 
+      request.message,
+      function() {
+        requestSnapshot.ref.remove();
+      }
+    );
+  }, function(error) {
+    console.error(error);
+  });
+};
+
+function sendNotificationToUser(username, message, onSuccess) {
+  request({
+    url: 'https://fcm.googleapis.com/fcm/send',
+    method: 'POST',
+    headers: {
+      'Content-Type' :' application/json',
+      'Authorization': 'key='+API_KEY
+    },
+    body: JSON.stringify({
+      notification: {
+        title: message
+      },
+      to : '/topics/user_'+username
+    })
+  }, function(error, response, body) {
+    if (error) { console.error(error); }
+    else if (response.statusCode >= 400) { 
+      console.error('HTTP Error: '+response.statusCode+' - '+response.statusMessage); 
+    }
+    else {
+      onSuccess();
+    }
+  });
+}
+
+listenForNotificationRequests();
